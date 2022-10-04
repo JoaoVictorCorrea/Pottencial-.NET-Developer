a9e0c010710deb5cf8760e74e209b91d3e9f0215
João Victor Corrêa <correa.joaov25@gmail.com>
2022-10-04 01:55:22 -0300

    Organização e Solução

---
+++ ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(){
+
+            Nome = "";
+        }
+
+        public void Apresentar(){
+
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
+        }
+    }
+}
---
+++ ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExemploFundamentos.Common.Models;
+
+Pessoa pessoa1 = new Pessoa();
+Calculadora calculadora = new Calculadora();
+
+pessoa1.Nome = "João";
+pessoa1.Idade = 19;
+pessoa1.Apresentar();
+
+calculadora.Somar(30, 10);
+calculadora.Subtrair(30, 10);
+calculadora.Multiplicar(30, 10);
+calculadora.Dividir(30, 10);
+calculadora.Potencia(2, 10);
+calculadora.Seno(45);
+calculadora.Coseno(45);
+calculadora.Tangente(45);
+calculadora.RaizQuadrada(49);
