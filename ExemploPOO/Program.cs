55fb168aef322a7fc37d8a42dac30ec631f57a26
João Victor Corrêa <correa.joaov25@gmail.com>
2022-10-29 22:35:45 -0300

    Herança e Polimorfismo com C#

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -7,10 +7,30 @@ p1.Idade = 19;
 
 p1.Apresentar();
 
+Console.WriteLine("");
+
 ContaCorrente c1 = new ContaCorrente(123, 1000);
 
 c1.ExibirSaldo();
-
 c1.Sacar(5000);
-
 c1.ExibirSaldo();
+
+Console.WriteLine("");
+
+Aluno a1 = new Aluno();
+
+a1.Nome = "Yuri";
+a1.Idade = 21;
+a1.Nota = 9.99;
+
+a1.Apresentar();
+
+Console.WriteLine("");
+
+Professor prof1 = new Professor();
+
+prof1.Nome = "Anderson";
+prof1.Idade = 33;
+prof1.Salario = 10.000M;
+
+prof1.Apresentar();
